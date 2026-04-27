f5d7cfcb470444f9805c258e7bffe657561b48f6
24641h <24641h@hvhs.school.nz>
2026-04-28 10:59:17 +1200

    28/4/26

--- t02_variables.js
+++ t02_variables.js
@@ -7,11 +7,12 @@ console.log("Car city")
 
 
   
-let name= Harsh
-let year= 2010
-let age=16
-let money=20
-console.log("i am Harsh + My year 2010 + My age 16 ");
+let name= "Harsh"
+let year= 2010;
+let age=16;
+let isstudent= true;
+let money=20;
+console.log("i am Harsh" + "My year 2010" + "My age 16 ");
 
 
 //Variables
