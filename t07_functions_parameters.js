58d52e1baf1e216fa0a557d1548d5c63e1e52fe3
HARSH252010 <24641h@hvhs.school.nz>
2026-05-13 01:21:01 +0000

    May 13, 2026, 1:21 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,43 @@
-/* Paste your code from task 6 here */
+/****************************
+functions parameters
+****************************/
+function writeline(cars){
+    // I am Harsh were born in India
+    output.innerHTML += "<p>Less code matters</p>";
+  }
+
+
+console.log("Running t05 javascript js")
+console.log("Hello World!")
+
+
+let Username= "Harsh"
+let Userage=16;
+let pocketmoney=20;
+let currentyear= 2036;
+
+const output = document.getElementById("spaceForJavaScriptOutput");
+
+
+
+
+
+/****************************
+Main code
+****************************/
+writeline()
+
+console.log("Hii + Harsh");
+console.log("As of 2010 +  i am 16 ");
+let yearborn = 2010 - 16;
+console.log("i am 16 years old " + 2010 );
+let userage = 16 + 2010;
+console.log("As of 2010 i am 16 years old");
+console.log("In 10 years i will be 26 years old");
+console.log("i have 20 dollors");
+console.log("Welcome to the shop")
+
+
+/****************************
+Functions
+****************************/
