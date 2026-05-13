cd9baa57477601f66c1a2db3841b3b21978547ab
HARSH252010 <24641h@hvhs.school.nz>
2026-05-13 01:06:23 +0000

    May 13, 2026, 1:06 AM

--- t06_functions.js
+++ t06_functions.js
@@ -17,7 +17,7 @@ let pocketmoney=20;
 let currentyear= 2036;
 
 const output = document.getElementById("spaceForJavaScriptOutput");
-output.innerHTML="<h2>Added by Javascript</h2>";
+
 
 
 /****************************
