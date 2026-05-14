d3c7bfade0acae934c93a46764d70f646050d70d
HARSH252010 <24641h@hvhs.school.nz>
2026-05-14 02:52:05 +0000

    May 14, 2026, 2:52 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,7 +1,7 @@
 /****************************
 functions parameters
 ****************************/
-function displayWelcome(Harsh){
+function displayWelcome(_name,_age){
     output.innerHTML += "<p>Welcome to the page"+_name+"</p>";
     output.innerHTML +="<p>you are"+_age+" years old</p>";
   }
