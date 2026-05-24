6fadb2d17c5048b8d061ed2580e8e22f514aa56a
HARSH252010 <24641h@hvhs.school.nz>
2026-05-24 21:18:10 +0000

    May 24, 2026, 9:18 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,51 @@
-/* Paste your code from task 7 here*/
+/****************************
+activate via button
+****************************/
+function displayWelcome(_name, _age){
+    output.innerHTML +="<p>Welcome to the page "+_name+" </p>";
+    output.innerHTML +="<p>you are "+_age+" years old</p>";
+  }
+
+
+console.log("Running t07 function parameters js");
+console.log("Hello World!");
+
+
+let Username= "Harsh"
+let Userage=16;
+let pocketmoney=20;
+let currentyear= 2036;
+
+const output = document.getElementById("spaceForJavaScriptOutput");
+
+
+
+
+
+/****************************
+Main code
+****************************/
+
+
+console.log("Hii + Harsh");
+console.log("As of 2010 +  i am 16 ");
+let yearborn = 2010 - 16;
+console.log("i am 16 years old " + 2010 );
+let userage = 16 + 2010;
+console.log("As of 2010 i am 16 years old");
+console.log("In 10 years i will be 26 years old");
+console.log("i have 20 dollors");
+console.log("Welcome to the shop");
+console.log("DisplayProduct");
+let userName = "Mr Quoc"
+let username = "Mr Harsh"
+displayWelcome("Mr Quoc", 16);
+displayWelcome("Mr Harsh", 16);
+
+console.log("chocolate bar:","$4");
+console.log("chips:","$3");
+console.log("Drink:","$2.50");
+
+/****************************
+Functions
+****************************/
