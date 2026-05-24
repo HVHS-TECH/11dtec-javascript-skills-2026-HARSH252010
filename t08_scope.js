d62f790cf62b3e493e9838b77feec6af4815e8c5
HARSH252010 <24641h@hvhs.school.nz>
2026-05-24 21:22:51 +0000

    May 24, 2026, 9:22 PM

--- t08_scope.js
+++ t08_scope.js
@@ -2,7 +2,7 @@ const output = document.getElementById("output");
 
 /* Read the slides, make a prediction, then uncomment this code.
 
-
+ 
 var myVar = 0;
 output.innerHTML = "Start: "+myVar+"<br>";
 
