e4fa713401424838e2f6b99b7a7bdab49f931011
HARSH252010 <24641h@hvhs.school.nz>
2026-05-25 22:59:51 +0000

    May 25, 2026, 10:59 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -36,16 +36,10 @@ console.log("As of 2010 i am 16 years old");
 console.log("In 10 years i will be 26 years old");
 console.log("i have 20 dollors");
 console.log("Welcome to the shop")
-console.log("DisplayProduct");
-console.log("chocolate bar:","$4");
-console.log("chips:","$3");
-console.log("Drink:","$2.50");
+
 
 
 
 /****************************
 Functions
 ****************************/
-function Start(){
-  
-}
