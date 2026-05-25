a4d5dfdf7b095d6d179d1f93b3db900d30717f26
HARSH252010 <24641h@hvhs.school.nz>
2026-05-25 22:39:47 +0000

    May 25, 2026, 10:39 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -36,14 +36,13 @@ console.log("As of 2010 i am 16 years old");
 console.log("In 10 years i will be 26 years old");
 console.log("I have 20 dollors");
 
-welcome 
-console.log("Welcome to the shop");
-
 let userName = "Mr Quoc"
 let username = "Mr Harsh"
 displayWelcome("Mr Quoc", 16);
 displayWelcome("Mr Harsh", 16);
 
+welcome() 
+console.log("Welcome to the shop");
 console.log("DisplayProduct");
 displayproduct("chocolate bar:","$4");
 displayproduct("chips:","$3");
