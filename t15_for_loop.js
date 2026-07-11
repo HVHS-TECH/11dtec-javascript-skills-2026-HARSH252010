96ed8765f2d09ef8811b7971a7a1a94945d9830c
HARSH252010 <24641h@hvhs.school.nz>
2026-07-11 03:43:43 +0000

    Jul 11, 2026, 3:43 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,16 @@
-/* Paste your code from the last task */
+/****************************
+for loop
+****************************/
+console.log("Running t15_for_loop.js");
+console.log("Working with for loops in JavaScript");
+
+//Variables
+
+
+/****************************
+Cars city
+****************************/
+  
+ /****************************
+  for loop
+  ****************************/
