6c7cfe664f7fa67c7dbf41bd5e4aba1e0a9e75dc
HARSH252010 <24641h@hvhs.school.nz>
2026-07-11 03:46:24 +0000

    Jul 11, 2026, 3:46 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -14,3 +14,30 @@ Main Code
  /****************************
   for loop
   ****************************/
+
+  function singSong(){
+
+    output.innerHTML = "";
+
+    const BOTTLES_FIELD = document.getElementById("bottlesField");
+
+    let bottles = Number(BOTTLES_FIELD.value);
+
+    console.log("Starting Loop");
+
+    for(let count = bottles; count > 0; count--){
+
+        output.innerHTML += "<p>" + count + " bottles of milk on the wall</p>";
+
+        output.innerHTML += "<p>" + count + " bottles of milk</p>";
+
+        output.innerHTML += "<p>If one of those bottles should happen to fall</p>";
+
+        output.innerHTML += "<p>" + (count - 1) + " bottles of milk on the wall.</p>";
+
+        output.innerHTML += "<br>";
+
+    }
+
+    console.log("Loop Ended");
+}
